df41cab39fbce27891731083f32ec6d6b5662091
Haerteleric <haertelgu@gmail.com>
2023-02-08 13:32:58 +0100

    added support for aarch64 Toolchain

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ARCH=$(arch)
 
 if [ ! -d "${scriptDir}/../gcc-arm-none-eabi" ]; then
     # Take action if $DIR exists. #
     echo "No Toolchain found!"
     echo "Downloading Arm M0+ Toolchain to:"
     echo "${scriptDir}/../gcc-arm-none-eabi"
-
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2 \
+    wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-${ARCH}-linux.tar.bz2" \
         -O ${scriptDir}/../gcc-arm-none-eabi.tar.bz2
     tar xfvj ${scriptDir}/../gcc-arm-none-eabi.tar.bz2
     rm ${scriptDir}/../gcc-arm-none-eabi.tar.bz2
